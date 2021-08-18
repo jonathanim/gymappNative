22e8b89bb97d12e74545b974c3654bab918e7a47
Jonathan <scrboy89@gmail.com>
2021-08-18 11:30:22 -0700

    finished updating UI in tracker component

--- components/List.js
+++ components/List.js
@@ -1,45 +1,66 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { Icon } from 'react-native-elements'
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Icon } from "react-native-elements";
 
-export default function List({ item, handleCompleted, removeItem, setSelectedId }) {
-    return (
-        <View key={item.id} onPress={() => setSelectedId(item.id)} style={styles.item}>
-            <View style={{ flex: 1 }}>
-                <Text style={item.completed ? styles.complete : styles.normal}>{item.name}</Text>
-            </View>
-            <View style={{ flex: 1, flexDirection: 'column' }}>
-                <Text style={item.completed ? styles.complete : styles.normal}>Sets:{item.sets}</Text>
-                <Text style={item.completed ? styles.complete : styles.normal}>Reps:{item.reps}</Text>
-            </View>
-            <View style={{ flex: 1, flexDirection: 'row' }}>
-                <Icon name='check' color="green" size={50} onPress={() => handleCompleted(item.id)} />
-                <Icon name='close' color="red" size={50} onPress={() => removeItem(item.id)} />
-            </View>
-        </View >
-    )
+export default function List({
+  item,
+  handleCompleted,
+  removeItem,
+  setSelectedId,
+}) {
+  return (
+    <View
+      key={item.id}
+      onPress={() => setSelectedId(item.id)}
+      style={styles.item}
+    >
+      <View style={{ flex: 1 }}>
+        <Text style={item.completed ? styles.complete : styles.normal}>
+          {item.name}
+        </Text>
+      </View>
+      <View style={{ flex: 1, flexDirection: "column" }}>
+        <Text style={item.completed ? styles.complete : styles.normal}>
+          Sets:{item.sets}
+        </Text>
+        <Text style={item.completed ? styles.complete : styles.normal}>
+          Reps:{item.reps}
+        </Text>
+      </View>
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <Icon
+          name="check"
+          color="green"
+          size={50}
+          onPress={() => handleCompleted(item.id)}
+        />
+        <Icon
+          name="close"
+          color="red"
+          size={50}
+          onPress={() => removeItem(item.id)}
+        />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-
-    item: {
-        flexDirection: 'row',
-        borderWidth: 2,
-        padding: 5,
-        marginTop: 5,
-        alignItems: 'center'
-
-    },
-    normal: {
-        color: 'black',
-        fontSize: 15,
-        margin: 5
-    },
-    complete: {
-        color: 'red',
-        fontSize: 15,
-        margin: 5,
-        textDecorationLine: 'line-through',
-    }
-
-})
+  item: {
+    flexDirection: "row",
+    borderWidth: 2,
+    padding: 5,
+    alignItems: "center",
+  },
+  normal: {
+    color: "black",
+    fontSize: 15,
+    margin: 5,
+  },
+  complete: {
+    color: "red",
+    fontSize: 15,
+    margin: 5,
+    textDecorationLine: "line-through",
+  },
+});
--- components/Tracker.js
+++ components/Tracker.js
@@ -1,243 +1,238 @@
-
-import React, { useState } from 'react'
-import { FlatList } from 'react-native';
+import React, { useState } from "react";
+import { FlatList } from "react-native";
 import {
-    View, TouchableOpacity, StyleSheet, TextInput, Text,
-} from 'react-native'
-import { Slider, Icon } from 'react-native-elements'
-import uuid from 'react-uuid'
-import List from './List';
-import DismissKeyboard from './DismissKeyboard';
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+  Text,
+} from "react-native";
+import { Slider, Icon } from "react-native-elements";
+import uuid from "react-uuid";
+import List from "./List";
+import DismissKeyboard from "./DismissKeyboard";
 
 function Tracker() {
+  const [exercises, setExercises] = useState([]);
+  const [name, setName] = useState("BenchPress");
+  const [sets, setSets] = useState(1);
+  const [reps, setReps] = useState(8);
+  const [id, setId] = useState(1);
+  const [selectedId, setSelectedId] = useState(null);
+  const [completed, setCompleted] = useState(false);
 
+  const handleSubmit = () => {
+    const addExercise = {
+      id: uuid(1),
+      name,
+      reps,
+      sets,
+      completed,
+    };
+    addItem(addExercise);
+    reset();
+  };
 
-    const [exercises, setExercises] = useState([]);
-    const [name, setName] = useState('BenchPress');
-    const [sets, setSets] = useState(1);
-    const [reps, setReps] = useState(8);
-    const [id, setId] = useState(1)
-    const [selectedId, setSelectedId] = useState(null);
-    const [completed, setCompleted] = useState(false);
+  function addItem(exercise) {
+    setExercises([...exercises, exercise]);
+  }
 
+  const removeItem = (id) => {
+    const removeItem = exercises.filter((ex) => ex.id !== id);
+    setExercises(removeItem);
+  };
 
-    const handleSubmit = () => {
-        const addExercise = {
-            id: uuid(1),
-            name,
-            reps,
-            sets,
-            completed
-        }
-        addItem(addExercise)
-        reset()
-    }
+  const reset = () => {
+    setName(name);
+    setSets(1);
+    setReps(8);
+    setCompleted(false);
+  };
 
-    function addItem(exercise) {
-        setExercises([...exercises, exercise])
-    }
+  const handleCompleted = (id) => {
+    const index = exercises.findIndex((ex) => ex.id === id);
+    const exercise = exercises.find((ex) => ex.id === id);
+    const newExercises = [...exercises].map((ex, idx) => {
+      if (idx === index) {
+        exercise.completed = !exercise.completed;
+      }
+      return ex;
+    });
 
-    const removeItem = (id) => {
-        const removeItem = exercises.filter(ex => ex.id !== id)
-        setExercises(removeItem)
-    }
+    console.log(newExercises);
+    setExercises(newExercises);
+  };
 
-    const reset = () => {
-        setName(name);
-        setSets(1);
-        setReps(8);
-        setCompleted(false)
-    }
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.titleContainer}>
+        <DismissKeyboard>
+          <Text style={styles.title}>Tracker</Text>
+        </DismissKeyboard>
+      </View>
+      <View style={{ flexDirection: "row" }}>
+        <TextInput
+          type="text"
+          name="name"
+          style={styles.input}
+          placeholder="add an exercise"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+        <TouchableOpacity style={styles.button} onPress={() => handleSubmit()}>
+          <Icon name="plus" type="font-awesome" color="white" size={30} />
+        </TouchableOpacity>
+      </View>
+      <View style={{ marginVertical: 10 }}>
+        <Slider
+          trackStyle={{
+            height: 5,
+          }}
+          thumbStyle={{
+            justifyContent: "center",
+            alignItems: "center",
+            height: 50,
+            width: 50,
+            backgroundColor: "white",
+            borderRadius: 15,
+            borderColor: "black",
+            borderWidth: 3,
+            padding: 3,
+          }}
+          thumbProps={{
+            children: (
+              <Icon
+                name="arrows-alt-h"
+                type="font-awesome-5"
+                color="red"
+                size={35}
+              />
+            ),
+          }}
+          value={sets}
+          name="sets"
+          minimumValue={1}
+          maximumValue={20}
+          onValueChange={(value) => setSets(Math.round(value))}
+        />
+        <Text style={styles.text}>Sets: {sets}</Text>
+        <Slider
+          thumbStyle={{
+            justifyContent: "center",
+            alignItems: "center",
+            height: 50,
+            width: 50,
+            backgroundColor: "white",
+            borderRadius: 15,
+            borderColor: "black",
+            borderWidth: 3,
+            padding: 3,
+          }}
+          thumbProps={{
+            children: (
+              <Icon
+                name="arrows-alt-h"
+                type="font-awesome-5"
+                color="red"
+                size={35}
+              />
+            ),
+          }}
+          name="reps"
+          value={reps}
+          minimumValue={1}
+          maximumValue={100}
+          onValueChange={(value) => setReps(Math.round(value))}
+        />
 
-    const handleCompleted = (id) => {
-        const index = exercises.findIndex((ex) => ex.id === id)
-        const exercise = exercises.find((ex) => ex.id === id)
-        const newExercises = [...exercises].map((ex, idx) => {
-            if (idx === index) {
-                exercise.completed = !exercise.completed
-            }
-            return ex
-        })
-
-        console.log(newExercises)
-        setExercises(newExercises)
-    }
-
-
-    return (
-
-        <View style={styles.mainContainer}>
-            <DismissKeyboard >
-                <View style={styles.titleContainer}>
-                    <Text style={styles.title}>
-                        Exercises Tracker
-                    </Text>
-                </View>
-            </DismissKeyboard>
-
-            <TextInput
-                type="text"
-                name='name'
-                style={styles.input}
-                placeholder="add an exercise"
-                value={name}
-                onChangeText={(text) => setName(text)}
+        <Text style={styles.text}>Reps:{reps}</Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          padding: 10,
+        }}
+      >
+        <FlatList
+          data={exercises}
+          renderItem={({ item }) => (
+            <List
+              item={item}
+              handleCompleted={handleCompleted}
+              removeItem={removeItem}
+              setSelectedId={setSelectedId}
             />
-            <Slider
-                trackStyle={{
-                    height: 5,
-                }}
-                thumbStyle={{
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: 50,
-                    width: 50,
-                    backgroundColor: 'white',
-                    borderRadius: 15,
-                    borderColor: 'black',
-                    borderWidth: 3,
-                    padding: 3,
-                }}
-                thumbProps={{
-                    children: <Icon
-                        name='arrows-alt-h'
-                        type='font-awesome-5'
-                        color='red'
-                        size={35}
-                    />
-                }}
-                value={sets}
-                name="sets"
-                minimumValue={1}
-                maximumValue={20}
-                onValueChange={value => setSets(Math.round(value))}
-            />
-            <Text style={styles.text}>Sets: {sets}</Text>
-            <Slider
-                thumbStyle={{
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: 50,
-                    width: 50,
-                    backgroundColor: 'white',
-                    borderRadius: 15,
-                    borderColor: 'black',
-                    borderWidth: 3,
-                    padding: 3,
-                }}
-                thumbProps={{
-                    children: <Icon
-                        name='arrows-alt-h'
-                        type='font-awesome-5'
-                        color='red'
-                        size={35}
-                    />
-                }}
-
-                name="reps"
-                value={reps}
-                minimumValue={1}
-                maximumValue={100}
-                onValueChange={value => setReps(Math.round(value))}
-            />
-            <Text style={styles.text}>Reps:{reps}</Text>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => handleSubmit()}
-            >
-                <Icon
-                    name='plus'
-                    type='font-awesome'
-                    color="white"
-                    size={30}
-                />
-            </TouchableOpacity>
-
-            <View style={{ flex: 1 }}>
-                <FlatList
-                    data={exercises}
-                    renderItem={({ item }) => <List item={item} handleCompleted={handleCompleted} removeItem={removeItem} setSelectedId={setSelectedId} />}
-                    extraData={selectedId}
-                    keyExtractor={item => item.id}
-                />
-            </View>
-
-        </View>
-
-    )
+          )}
+          extraData={selectedId}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+    </View>
+  );
 }
 
-
-
-
-
-export default Tracker
-
-
+export default Tracker;
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        flex: 1,
-        padding: 20,
-        justifyContent: 'center',
-        alignContent: 'center',
-        backgroundColor: 'aqua',
-
+  mainContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "center",
+    alignContent: "center",
+    backgroundColor: "aqua",
+  },
+  titleContainer: {
+    marginTop: 40,
+    flex: 1,
+  },
+  title: {
+    fontSize: 40,
+    textAlign: "center",
+    color: "#ffdf6c",
+    textShadowColor: "black",
+    textShadowOffset: {
+      width: 3,
+      height: 3,
     },
-    titleContainer: {
-        marginTop: 40
-    },
-    title: {
-        fontSize: 40,
-        textAlign: 'center',
-        color: '#ffdf6c',
-        textShadowColor: 'black',
-        textShadowOffset: {
-            width: 3,
-            height: 3
-        },
-        textShadowRadius: 10
-    },
-    display: {
-        flex: 1,
-        marginTop: 80,
-        textAlign: 'center',
-        borderWidth: 5,
-        padding: 10,
-        justifyContent: 'center',
-        borderRadius: 15,
-        backgroundColor: '#ffdf6c',
-    },
-    button: {
-        borderColor: 'white',
-        borderWidth: 3,
-        backgroundColor: 'black',
-        paddingVertical: 10,
-        borderRadius: 15,
-        marginTop: 10
-    },
-    controls: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    input: {
-        height: 50,
-        borderWidth: 3,
-        textAlign: "center",
-        backgroundColor: 'white',
-        marginVertical: 10
-    },
-    item: {
-        flexDirection: 'row',
-        borderWidth: 2,
-        padding: 5,
-        marginTop: 5,
-
-    },
-    normal: {
-        color: 'black',
-    }
-
-})
+    textShadowRadius: 10,
+  },
+  display: {
+    flex: 1,
+    marginTop: 80,
+    textAlign: "center",
+    borderWidth: 5,
+    padding: 10,
+    justifyContent: "center",
+    borderRadius: 15,
+    backgroundColor: "#ffdf6c",
+  },
+  button: {
+    height: 50,
+    borderWidth: 3,
+    justifyContent: "center",
+    alignContent: "center",
+    textAlign: "center",
+    backgroundColor: "black",
+    marginVertical: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+  },
+  controls: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    height: 50,
+    borderWidth: 3,
+    textAlign: "center",
+    backgroundColor: "white",
+    marginVertical: 10,
+    width: "80%",
+  },
+  text: {
+    color: "black",
+    marginVertical: 10,
+  },
+});
